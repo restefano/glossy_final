1ecc1abc39145d40de88063aedd16fea5a641b89
Ricardo Estefano Rosa <ricardo@risotecnologia.com.br>
2013-08-27 20:52:03 -0300

    revserver

--- controllers/front/PageNotFoundController.php
+++ controllers/front/PageNotFoundController.php
@@ -7,7 +7,7 @@
 * This source file is subject to the Open Software License (OSL 3.0)
 * that is bundled with this package in the file LICENSE.txt.
 * It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/osl-3.0.php
+* http://opensource.or...ses/osl-3.0.php
 * If you did not receive a copy of the license and are unable to
 * obtain it through the world-wide-web, please send an email
 * to license@prestashop.com so we can send you a copy immediately.
@@ -18,43 +18,43 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2013 PrestaShop SA
-*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
+* @author PrestaShop SA <contact@prestashop.com>
+* @copyright 2007-2013 PrestaShop SA
+* @license	http://opensource.or...ses/osl-3.0.php Open Software License (OSL 3.0)
+* International Registered Trademark & Property of PrestaShop SA
 */
 
 class PageNotFoundControllerCore extends FrontController
+{ /*ini sudah diganti */
+public $php_self = '404';
+public $page_name = 'pagenotfound';
+
+/**
+* Assign template vars related to page content
+* @see FrontController::initContent()
+*/
+public function initContent()
 {
-	public $php_self = '404';
-	public $page_name = 'pagenotfound';
+/*here you should change your homepage */
+header("Location: http://www.glossyme.com.br/");
 
-	/**
-	 * Assign template vars related to page content
-	 * @see FrontController::initContent()
-	 */
-	public function initContent()
-	{
-		header('HTTP/1.1 404 Not Found');
-		header('Status: 404 Not Found');
+if (in_array(Tools::strtolower(substr($_SERVER['REQUEST_URI'], -3)), array('png', 'jpg', 'gif')))
+{
+header('Content-Type: image/gif');
+readfile(_PS_IMG_DIR_.'404.gif');
+exit;
+}
+elseif (in_array(Tools::strtolower(substr($_SERVER['REQUEST_URI'], -3)), array('.js', 'css')))
+exit;
 
-		if (in_array(Tools::strtolower(substr($_SERVER['REQUEST_URI'], -3)), array('png', 'jpg', 'gif')))
-		{
-			header('Content-Type: image/gif');
-			readfile(_PS_IMG_DIR_.'404.gif');
-			exit;
-		}
-		elseif (in_array(Tools::strtolower(substr($_SERVER['REQUEST_URI'], -3)), array('.js', 'css')))
-			exit;
+parent::initContent();
 
-		parent::initContent();
+$this->setTemplate(_PS_THEME_DIR_.'404.tpl');
+}
 
-		$this->setTemplate(_PS_THEME_DIR_.'404.tpl');
-	}
-	
-	public function canonicalRedirection($canonical_url = '')
-	{
-		// 404 - no need to redirect to the canonical url
-	}
+public function canonicalRedirection($canonical_url = '')
+{
+// 404 - no need to redirect to the canonical url
+}
 }
 
