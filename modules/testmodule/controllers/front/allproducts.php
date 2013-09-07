ae02b9ef4d4825e4ad80b63d159d6861a6475a52
Ricardo Estefano Rosa <ricardo@dba.eng.br>
2013-09-07 16:22:44 -0300

    revisao direta

--- modules/testmodule/controllers/front/allproducts.php
+++ modules/testmodule/controllers/front/allproducts.php
@@ -12,6 +12,14 @@ class testmoduleAllproductsModuleFrontController extends ModuleFrontController
 	public function initContent()
 	{
     	parent::initContent();
+    	$products_partial = Product::getProducts($this->context->language->id, 0, 30, 'name', 'asc');
+    	$products = Product::getProductsProperties($this->context->language->id, $products_partial);
+ 
+    	$this->context->smarty->assign(array(
+        'products' => $products,
+        'homeSize' => Image::getSize('home_default')
+    	));
+
     	$this->setTemplate('allproducts.tpl');
 	}   	
 
