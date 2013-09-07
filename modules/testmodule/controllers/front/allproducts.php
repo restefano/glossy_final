635639f1f3ef9b0d28e83e4b5b7ec08e95a55a17
Ricardo Estefano Rosa <ricardo@dba.eng.br>
2013-09-07 16:04:33 -0300

    revisao direta

--- modules/testmodule/controllers/front/allproducts.php
+++ modules/testmodule/controllers/front/allproducts.php
@@ -2,6 +2,13 @@
 
 class testmoduleAllproductsModuleFrontController extends ModuleFrontController
 {
+
+public function init()
+{
+	$this->page_name = 'ver_todos'; // page_name and body id
+    parent::init();
+}
+
  
 }
 
